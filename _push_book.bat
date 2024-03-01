d8eec4078f2ca7bd9be1a5c3e551fed2f03fdbbe
Lê Huỳnh Đức PTCL <duclh3@f88.vn>
2024-03-01 09:53:12 +0700

    first commit

---
+++ _push_book.bat
@@ -0,0 +1,7 @@
+git pull
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
+
+git add .
+git commit -m "push book"
+git push
